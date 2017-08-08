d8546ee74a75320e7e9b45ced3c9ef9bd627ba9c
Wang Siyuan <2343257@qq.com>
2017-08-08 16:24:15 -0500

    N/A

--- src/main/java/com/water/repository/SensorDataRepository.java
+++ src/main/java/com/water/repository/SensorDataRepository.java
@@ -3,6 +3,7 @@ package com.water.repository;
 import com.water.entity.SensorData;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 /**
@@ -14,5 +15,9 @@ public interface SensorDataRepository extends CrudRepository<SensorData, Long> {
     List<SensorData> findAllBySensorId(Long sensorId);
 
     List<SensorData> findAll();
+
+    List<SensorData> findSensorDataByTimeAfter(Timestamp timestamp);
+
+    SensorData findFirstBySensorIdOrderByTimeDesc(Long sensorId);
 
 }
